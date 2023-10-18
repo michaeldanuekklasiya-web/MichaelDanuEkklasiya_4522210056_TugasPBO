6dc3c03ad2191a3a979675a92a6e62d38bd03096
Michael Danu Ekklasiya <michaeldanuekklasiya@gmail.com>
2023-10-18 21:58:50 +0700

    Update Terbaru Fix

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import Matriks.*;
-import Pecahan.*;
+import Matriks.Pecahan;
+import Matriks.Matrik;
 
 public class Main {
     public static void main(String[] args) {
-        Pecahan pecahan1 = new Pecahan(1, 2);
-        Pecahan pecahan2 = new Pecahan(3, 4);
+        System.out.println("MATERI PECAHAN");
+        Pecahan pecahan1 = new Pecahan(6, 2);
+        Pecahan pecahan2 = new Pecahan(4, 8);
 
         System.out.println("Pecahan 1: " + pecahan1);
         System.out.println("Pecahan 2: " + pecahan2);
@@ -13,20 +14,32 @@ public class Main {
         Pecahan hasilKurang = pecahan1.kurang(pecahan2);
         Pecahan hasilKali = pecahan1.kali(pecahan2);
         Pecahan hasilBagi = pecahan1.bagi(pecahan2);
+        System.out.println();
 
         System.out.println("Penjumlahan : " + pecahan1 + " + " + pecahan2 + " = " + hasilTambah);
         System.out.println("Pengurangan : " + pecahan1 + " - " + pecahan2 + " = " + hasilKurang);
         System.out.println("Perkalian   : " + pecahan1 + " : " + pecahan2 + " = " + hasilKali);
         System.out.println("Pembagian   : " + pecahan1 + " x " + pecahan2 + " = " + hasilBagi);
+        System.out.println();
 
-        Pecahan p1 = new Pecahan(1, 2);
-        Pecahan p2 = new Pecahan(1, 3);
-        Pecahan p3 = new Pecahan(1, 4);
-        Pecahan p4 = new Pecahan(1, 5);
+        System.out.println("MATERI PECAHAN MATRIX");
+        Pecahan p1 = new Pecahan(3, 6);
+        Pecahan p2 = new Pecahan(2, 5);
+        Pecahan p3 = new Pecahan(3, 5);
+        Pecahan p4 = new Pecahan(2, 4);
 
+        // Mengisi matriks1 dan matriks2 dengan Pecahan
         Matrik matriks1 = new Matrik(2, 2);
-
         Matrik matriks2 = new Matrik(2, 2);
+
+        Matriks.Pecahan[][] data = {
+                {p1, p2},
+                {p3, p4}
+        };
+
+        matriks1.setData(data);
+        matriks2.setData(data);
+
 
         Matrik hasilTambah1 = matriks1.tambah(matriks2);
         Matrik hasilKurang1 = matriks1.kurang(matriks2);
@@ -39,10 +52,10 @@ public class Main {
         System.out.println(matriks2.toString());
 
         System.out.println("Hasil Penjumlahan Matriks:");
-        System.out.println(hasilTambah.toString());
+        System.out.println(hasilTambah1.toString());
 
         System.out.println("Hasil Pengurangan Matriks:");
-        System.out.println(hasilKurang.toString());
+        System.out.println(hasilKurang1.toString());
 
         System.out.println("Transpose Matriks 1:");
         System.out.println(hasilTranspose.toString());
